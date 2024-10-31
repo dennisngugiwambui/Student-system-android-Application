944bcc64aad9c2368193b4af305a5ee62408031f
Denno <dellyit001@gmail.com>
2024-10-31 09:52:00 -0700

    initial commit

---
+++ StudentSystem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
